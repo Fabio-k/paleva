72571425d6362eb6df6598f32d7f4eb1ea8e883d
fabio-k <fabiokmain@gmail.com>
2024-10-31 15:39:49 -0300

    Add restaurant association to dishes test

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -4,13 +4,17 @@ RSpec.describe Dish, type: :model do
   describe '#valid?' do
     context 'presence' do
       it 'false when name is empty' do
-        dish = Dish.new(name: '', description: 'Feijão preto cozido com carnes suínas e bovinas, servido com arroz branco, couve refogada, farofa crocante e fatias de laranja. Um clássico brasileiro, perfeito para momentos especiais.')
+        admin = Admin.create!(cpf: CPF.generate, name: 'Sergio', last_name: 'Oliveira', email: 'sergio@email.com', password: 'senha123senha')
+        restaurant = Restaurant.create!(brand_name: 'Burger King', corporate_name: 'Burger King LTDA', registration_number: CNPJ.generate, street: 'Avenida cívica', address_number: '103', city: 'Mogi das Cruzes', state: 'São Paulo', phone_number: '1197894339', email: 'burgerking@email.com', admin: admin)
+        dish = Dish.new(name: '', description: 'Feijão preto cozido com carnes suínas e bovinas, servido com arroz branco, couve refogada, farofa crocante e fatias de laranja. Um clássico brasileiro, perfeito para momentos especiais.', restaurant: restaurant)
         
         expect(dish.valid?).to eq false
       end
 
       it 'false when description is empty' do
-        dish = Dish.new(name: 'Feijoada', description: '')
+        admin = Admin.create!(cpf: CPF.generate, name: 'Sergio', last_name: 'Oliveira', email: 'sergio@email.com', password: 'senha123senha')
+        restaurant = Restaurant.create!(brand_name: 'Burger King', corporate_name: 'Burger King LTDA', registration_number: CNPJ.generate, street: 'Avenida cívica', address_number: '103', city: 'Mogi das Cruzes', state: 'São Paulo', phone_number: '1197894339', email: 'burgerking@email.com', admin: admin)
+        dish = Dish.new(name: 'Feijoada', description: '', restaurant: restaurant)
         
         expect(dish.valid?).to eq false
       end
